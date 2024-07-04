3f941e2545a747413efdd1a2103eb50866748373
Thanh Tu Nguyen <thanh_tu_nguyen@ext.carrefour.com>
2024-07-04 16:48:34 +0200

    change backend url

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,3 @@
 import reflex as rx
 
-config = rx.Config(
-    app_name="hackathon", api_url="https://cf5f-81-66-213-244.ngrok-free.app"
-)
+config = rx.Config(app_name="hackathon", api_url="http://68.183.243.68:8000")
